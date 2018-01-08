74192185731f2193829d4360cb3377bdfe78f823
geekman9097 <powerplanets@aol.com>
2018-01-08 16:57:07 -0500

    Dry Distiller Crafting Recipes
    I'm told this will be made relevant soon

--- src/java/com/sinesection/logisticraft/crafting/DryDistillerCraftingRecipe.java
+++ src/java/com/sinesection/logisticraft/crafting/DryDistillerCraftingRecipe.java
@@ -1,7 +1,5 @@
 package com.sinesection.logisticraft.crafting;
 
-import java.util.Arrays;
-
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fluids.FluidStack;
 
@@ -16,7 +14,7 @@ public class DryDistillerCraftingRecipe {
 		if(input == null) 
 			throw new IllegalArgumentException("Input ItemStack cannot be null!");
 		if(outputs.length > 4 || outputs.length == 0)
-			throw new IllegalArgumentException("Amout of output ItemStacks is illegal: " + outputs.length + ", Min: 1, Max: 3");
+			throw new IllegalArgumentException("Amout of output ItemStacks is illegal: " + outputs.length + ", Min: 1, Max: 4");
 		if(fluidOutput != null && !fractionatorRequired)
 			throw new IllegalArgumentException("Can only have fluid output when the Fractionator is required!");
 		
@@ -25,5 +23,7 @@ public class DryDistillerCraftingRecipe {
 		this.fluidOutput = fluidOutput;
 		this.fractionatorRequired = fractionatorRequired;
 	}
+	
+	
 
 }
