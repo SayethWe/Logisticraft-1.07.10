bd7fa7b0055ab43feb27b8891fb072fd4a17e4f5
geekman9097 <powerplanets@aol.com>
2018-01-10 19:46:17 -0500

    Power System FRamework

---
+++ src/java/com/sinesection/logisticraft/power/LogisticraftHeatBlock.java
@@ -0,0 +1,47 @@
+package com.sinesection.logisticraft.power;
+
+import com.sinesection.logisticraft.block.LogisticraftBlock;
+
+import net.minecraft.block.material.Material;
+
+public abstract class LogisticraftHeatBlock extends LogisticraftBlock {
+
+	protected int temp;
+	protected int energy;
+	
+	protected LogisticraftHeatBlock(String name, String textureName, String registryName, Material mat) {
+		super(name, textureName, registryName, mat);
+	}
+
+	public LogisticraftHeatBlock(String universalName) {
+		super(universalName, Material.iron);
+	}
+	
+	public int getCurrentTemp() {
+		return temp;
+	}
+	
+	public int getEnergy() {
+		return energy;
+	}
+	
+	public boolean takeEnergy(int tempRequested) {
+		if(tempRequested <= temp) {
+			energy -= tempRequested;
+			ensureTempStatus();
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	public void addEnergy(int tempIn) {
+		energy += tempIn;
+		temp = Math.max(temp, tempIn);
+	}
+	
+	private void ensureTempStatus() {
+		if(temp > energy) temp = energy;
+	}
+
+}
