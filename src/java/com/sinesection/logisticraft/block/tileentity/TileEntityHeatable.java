6d6dcd73a9685394cc7528c280afbe6d2c5052f8
Richard Abbott <richie0415@hotmail.com>
2018-01-21 12:42:51 -0500

    Stuff

--- src/java/com/sinesection/logisticraft/block/tileentity/TileEntityHeatable.java
+++ src/java/com/sinesection/logisticraft/block/tileentity/TileEntityHeatable.java
@@ -1,14 +1,21 @@
 package com.sinesection.logisticraft.block.tileentity;
 
 import com.sinesection.logisticraft.power.IHeatable;
+import com.sinesection.logisticraft.registrars.HeatProperties;
 
 import net.minecraft.tileentity.TileEntity;
 import net.minecraftforge.common.util.ForgeDirection;
 
 public class TileEntityHeatable extends TileEntity implements IHeatable {
 	
-	private float temp;
-
+	private float temp, energy;
+	private final HeatProperties heatProperties;
+	
+	public TileEntityHeatable(HeatProperties hp) {
+		this.heatProperties = hp;
+	}
+	
+	@Override
 	public float getCurrentTemp() {
 		return temp;
 	}
@@ -17,48 +24,49 @@ public class TileEntityHeatable extends TileEntity implements IHeatable {
 		super.updateEntity();
 	}
 
-	@Override
 	public float getRequiredTemp() {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	@Override
 	public float getCurrentEnergy() {
-		// TODO Auto-generated method stub
+		return energy;
+	}
+
+	@Override
+	public float getMaxEnergy() {
 		return 0;
 	}
 
 	@Override
 	public boolean takeEnergy(float tempRequested, boolean changeTemp, ForgeDirection side) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
-	public void addEnergy(float tempIn, ForgeDirection side) {
-		// TODO Auto-generated method stub
-		
+	public boolean addEnergy(float tempIn, boolean changeTemp, ForgeDirection side) {
+		return false;
 	}
 
 	@Override
 	public boolean isReceivingEnergy() {
+		return false;
+	}
+
+	@Override
+	public boolean isLosingEnergy() {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public boolean canInputFrom(ForgeDirection dir) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public boolean canOutputTo(ForgeDirection dir) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 	
-	
-
 }
--- src/java/com/sinesection/logisticraft/power/IHeatable.java
+++ src/java/com/sinesection/logisticraft/power/IHeatable.java
@@ -7,9 +7,11 @@ public interface IHeatable {
 	public float getCurrentTemp();
 	public float getRequiredTemp();
 	public float getCurrentEnergy();
-	public boolean takeEnergy(float tempRequested, boolean changeTemp, ForgeDirection side);
-	public void addEnergy(float tempIn, ForgeDirection side);
+	public float getMaxEnergy();
 	public boolean isReceivingEnergy();
+	public boolean isLosingEnergy();
+	public boolean takeEnergy(float tempRequested, boolean changeTemp, ForgeDirection side);
+	public boolean addEnergy(float tempIn, boolean changeTemp, ForgeDirection side);
 	public boolean canInputFrom(ForgeDirection dir);
 	public boolean canOutputTo(ForgeDirection dir);
 
