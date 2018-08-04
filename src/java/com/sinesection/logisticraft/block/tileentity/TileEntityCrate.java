d47e1bcd067f60ac981dc2c49ae4b1674bb5cdf0
geekman9097 <powerplanets@aol.com>
2018-08-04 09:48:35 -0600

    Crate Tile entity boilerplate

--- src/java/com/sinesection/logisticraft/block/tileentity/TileEntityCrate.java
+++ src/java/com/sinesection/logisticraft/block/tileentity/TileEntityCrate.java
@@ -1,5 +1,102 @@
 package com.sinesection.logisticraft.block.tileentity;
 
-public class TileEntityCrate extends LogisticraftTileEntity {
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.ISidedInventory;
+import net.minecraft.item.ItemStack;
+
+public class TileEntityCrate extends LogisticraftTileEntity implements ISidedInventory{
 	public static final int NUM_SLOTS = 1;
+	public static final int SlOT_INVENTORY = 0;
+	
+	
+	//Fields
+	private ItemStack[] slots = new ItemStack[NUM_SLOTS];
+	
+	@Override
+	public int getSizeInventory() {
+		return NUM_SLOTS;
+	}
+	
+	@Override
+	public ItemStack getStackInSlot(int slot) {
+		if (slot < 0 || slot > getSizeInventory())
+			return null;
+		return this.slots[slot];
+	}
+	
+	@Override
+	public int getInventoryStackLimit() {
+		return 16;
+	}
+	
+	@Override
+	public boolean isUseableByPlayer(EntityPlayer player) {
+		return this.worldObj.getTileEntity(this.xCoord, this.yCoord, this.zCoord) != this ? false : player.getDistanceSq((double) this.xCoord + 0.5D, (double) this.yCoord + 0.5D, (double) this.zCoord + 0.5D) <= 64.0D;
+	}
+
+	@Override
+	public ItemStack decrStackSize(int slot, int amt) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ItemStack getStackInSlotOnClosing(int slot) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setInventorySlotContents(int slot, ItemStack item) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getInventoryName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean hasCustomInventoryName() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void openInventory() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void closeInventory() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean isItemValidForSlot(int p_94041_1_, ItemStack p_94041_2_) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int[] getAccessibleSlotsFromSide(int p_94128_1_) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean canInsertItem(int p_102007_1_, ItemStack p_102007_2_, int p_102007_3_) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean canExtractItem(int p_102008_1_, ItemStack p_102008_2_, int p_102008_3_) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 }
